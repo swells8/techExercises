0756e4c3099fa2fdd127ef02ca4a4acd4d04becc
Gianpierre Terry <gterry@unomaha.edu>
2025-06-12 21:53:16 -0500

    file structure changes and clean-up

--- src/app/components/ui/NavBar.tsx
+++ src/app/components/ui/NavBar.tsx
@@ -1,5 +1,5 @@
 
-
+import React from 'react';
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import Link from 'next/link';
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-
+import React from 'react';
 import NavBar from "./components/ui/NavBar";
 import Main from "./components/ui/Main";
 
 export default function Home() {
   return(<>
     <NavBar></NavBar>
-
-    
     <Main></Main>
     
     </>
